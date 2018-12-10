6d9e0b0f05f50f83e03a538d8d7b27c093e20735
Daniel Mears <clockworkdaniel@gmail.com>
2018-12-10 20:55:45 +0000

    remove unneeded author controllers/routes

--- src/server/controllers/authorController.js
+++ src/server/controllers/authorController.js
@@ -1,8 +1,6 @@
-/* eslint func-names: "off" */
 var Author = require('../models/author');
 
 exports.getAuthors = function (req, res) {
-  res.send('get Authors');
   Author.find({}, (err, authors) => {
     if (err) {
       return res.status(500).json({ message: err.message });
@@ -12,42 +10,10 @@ exports.getAuthors = function (req, res) {
 };
 
 exports.getAuthor = function (req, res) {
-  res.send('get Author');
   Author.findOne({ id: req.params.id }).exec((err, author) => {
     if (err) {
-      res.status(500).send(err);
+      return res.status(500).json({ message: err.message });
     }
     res.json({ author });
   });
 };
-
-
-exports.postAuthor = function (req, res) {
-  res.send('post Author');
-  var author = req.body;
-  Author.create(author, (err, author) => {
-    if (err) {
-      return res.status(500).json({ err: err.message });
-    }
-    res.json({ 'author': author, message: 'Author Created' });
-  });
-};
-
-exports.putAuthor = function (req, res) {
-  res.send('put Author');
-  // var id = req.params.id;
-  // var author = req.body;
-  // if (author && Author._id !== id) {
-  //   return res.status(500).json({ err: "Ids don't match!" });
-  // }
-  // Author.findByIdAndUpdate(id, author, { new: true }, (err) => {
-  //   if (err) {
-  //     return res.status(500).json({ err: err.message });
-  //   }
-  //   res.json({ 'author': author, message: 'Author Updated' });
-  // });
-};
-
-exports.deleteAuthor = function(req, res) {
-    res.send('delete Author');
-}
--- src/server/routes/author.js
+++ src/server/routes/author.js
@@ -5,11 +5,7 @@ var AuthorController = require('../controllers/authorController');
 var router = express.Router();
 
 router.route('/authors').get(AuthorController.getAuthors);
-
-router.route('/author/create').post(AuthorController.postAuthor);
-
 router.route('/author/:id').get(AuthorController.getAuthor);
-router.route('/author/:id/update').put(AuthorController.putAuthor);
-router.route('/authpr/:id/delete').delete(AuthorController.deleteAuthor);
+
 
 module.exports = router;
