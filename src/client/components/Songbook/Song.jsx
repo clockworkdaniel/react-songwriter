1041216b198b13d8c67cb2857dd6ff5a9320bc50
Daniel Mears <clockworkdaniel@gmail.com>
2018-12-19 17:18:07 +0000

    card template

--- src/client/components/Songbook/Song.jsx
+++ src/client/components/Songbook/Song.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import niceDate from '../../functions/niceDate';
 
 
 export default function Song({
@@ -11,10 +12,18 @@ export default function Song({
   }
 
   return (
-    <li className="song">
-      <h3 className="song__title controls__container">
-        {song.title}
+    <Link to={`/song/${song._id}`}>
+      <li className="song controls__container">
+        <h3 className="song__title ">
+          {song.title}
+        </h3>
         <div className="controls">
+          <button
+            className="controls__edit"
+            type="button"
+          >
+            <span className="icon-pencil" />
+          </button>
           <button
             className="controls__delete"
             onClick={handleDeleteSong}
@@ -22,35 +31,26 @@ export default function Song({
           >
             <span className="icon-cross" />
           </button>
-          <Link to={`/song/${song._id}`}>
-            <button
-              className="controls__edit"
-              type="button"
-            >
-              <span className="icon-pencil" />
-            </button>
-          </Link>
         </div>
         {author && (
-          <Link to={`/author/${author._id}`}>
-            <h4 className="song__author-name">
-              {author.name}
-            </h4>
-          </Link>
+        <Link to={`/author/${author._id}`}>
+          <h4 className="song__author-name">
+            {author.name}
+          </h4>
+        </Link>
         )}
-        {(orderLogic === 'created') && (
+        {(orderLogic === 'created') ? (
           <p className="song__date">
-            <span className="song__date-type">Created on:</span>
-            <span className="song__date-date">{song.created}</span>
+            <span className="song__date-type">Created on: </span>
+            <span className="song__date-date">{niceDate(song.created)}</span>
+          </p>
+        ) : (
+          <p className="song__date">
+            <span className="song__date-type">Modified on: </span>
+            <span className="song__date-date">{niceDate(song.modified)}</span>
           </p>
         )}
-        {(orderLogic === 'modified') && (
-          <p className="song__date">
-            <span className="song__date-type">Modified on:</span>
-            <span className="song__date-date">{song.modified}</span>
-          </p>
-        )}
-      </h3>
-    </li>
+      </li>
+    </Link>
   );
 }
