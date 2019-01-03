2fda516edc2bb753e9864f3b13fe4b98fe0c05a3
Daniel Mears <clockworkdaniel@gmail.com>
2019-01-03 09:44:08 +0000

    sass hot module reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   entry: './src/client/index.js',
@@ -32,7 +33,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
         ],
